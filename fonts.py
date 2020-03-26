e03123873618f1810cb394f45d6a389e9d7435ec
Dinesh <dinu1763@gmail.com>
2020-03-26 14:17:46 +0530

    Update fonts.py

--- fonts.py
+++ fonts.py
@@ -2,13 +2,13 @@ import glob, os, shutil
 
 
 source_dir = os.path.dirname(os.path.realpath(__file__))
-dest_dir = r'H:\UberEats App + Backend\adobe\Adobe.Font.Folio.Collection.v11.0.otf_p30download.com\Western Fonts\dir'
+dest_dir = path to destination directory
 
 for root, dirs, files in os.walk(".", topdown=False):
     for name1 in dirs:
         name_ = os.path.join(root, name1)
 
-        files = glob.iglob(os.path.join(name_, "*.otf"))
+        files = glob.iglob(os.path.join(name_, "*.<file-type-extension>"))
         for file in files:
             if os.path.isfile(file):
                 if file in dest_dir:
